4bdf63727add9060c8f1bd92d2ad528a6ae5b4b3
vsuzdaltsev <suzdaltsev_v@mail.ru>
2020-04-22 19:24:26 +0300

    rubocop

--- kube/recipes/kill_pods.rb
+++ kube/recipes/kill_pods.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
 
 ruby_block 'kill running pods' do
   block do
-    running_pods = -> { `kubectl get pods`.split.select { |pod| pod if pod.include? 'postgres' or pod.include? 'web-api' } }
+    running_pods = lambda {
+      `kubectl get pods`.split.select do |pod|
+        pod if pod.include?('postgres') || pod.include?('web-api')
+      end
+    }
     running_pods.call.each do |pod|
       `kubectl delete pods #{pod}`
     end
